3598fa6d9cc8ae7939366cd6590ab96ed7985852
Shem Baijoo <shem.baijoo@gmail.com>
2022-12-08 12:49:15 -0500

    Base API controllers

---
+++ CapstoneAPI/Controllers/StudentController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CapstoneAPI.Controllers
+{
+    [Route("api/students")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+    }
+}
